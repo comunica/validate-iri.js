ce0645953bc423b928fe4db9a184a576263d8960
Daniel Beeke <mail@danielbeeke.nl>
2021-11-24 14:22:21 +0100

    Starting small with a regex for scanning for html entities and the regex Ruben uses in jsonld-context-parser.js

--- lib/Validate.ts
+++ lib/Validate.ts
@@ -1,8 +1,41 @@
+export enum Strategies {
+  SkipValidation = 'skip-validation',
+  Pragmatic = 'pragmatic',
+  Specification = 'specification',
+}
+
+const defaultStrategy = Strategies.Pragmatic;
+
+export class HTMLEntityFoundError extends Error {}
+export class NotImplementedError extends Error {}
+export class InvalidIriError extends Error {}
+
+const IRI_REGEX = /^([A-Za-z][\d+-.A-Za-z]*|_):[^ "#<>[\\\]`{|}]*(#[^#]*)?$/u;
+const HTML_ENTITIES_REGEX = /&(?:#x[\da-f]+|#\d+|[\da-z]+);?/gui;
+
 /**
  * Validate a given IRI.
  * @param {string} a string that may be an IRI.
  * @return {Error | undefined} An error if the IRI is invalid, or undefined if it is valid.
  */
-export function validateIri(iri: string): Error | undefined {
-  return new Error('validateIri has not been implemented yet');
+export function validateIri(iri: string, strategy: Strategies = defaultStrategy): Error | undefined {
+  if (strategy === Strategies.SkipValidation) {
+    return;
+  }
+
+  if (strategy === Strategies.Pragmatic) {
+    const isValidIri = IRI_REGEX.test(iri);
+    if (!isValidIri) {
+      return new InvalidIriError('The given IRI is not a valid IRI.');
+    }
+
+    const foundEntity = HTML_ENTITIES_REGEX.test(iri);
+    if (foundEntity) {
+      return new HTMLEntityFoundError('Found HTML entities. Suggestion: double encode these IRI\'s.');
+    }
+  }
+
+  if (strategy === Strategies.Specification) {
+    return new NotImplementedError('validateIri with the given strategy: "Specification" has not been implemented yet');
+  }
 }
--- test/Validate-test.ts
+++ test/Validate-test.ts
@@ -1,1 +1,28 @@
-// TODO
+import { validateIri, Strategies, HTMLEntityFoundError, NotImplementedError, InvalidIriError } from '../lib/Validate';
+
+describe('strategies and basic validation', () => {
+  test(`scanning for HTML entities where they exist`, () => {
+    expect(validateIri('https://example.com/a:&#xA;', Strategies.Pragmatic)).toBeInstanceOf(HTMLEntityFoundError);
+  });
+
+  test(`scanning for HTML entities where they do not exist`, () => {
+    expect(validateIri('https://example.com/lorëm-ipsum', Strategies.Pragmatic)).toBe(undefined);
+  });
+
+  test(`using implicit pragmatic strategy`, () => {
+    expect(validateIri('https://example.com/a:&#xA;')).toBeInstanceOf(HTMLEntityFoundError);
+  });
+
+  test(`an invalid iri using implicit pragmatic strategy`, () => {
+    expect(validateIri('example-of-invalid-uri')).toBeInstanceOf(InvalidIriError);
+  });
+
+  test(`using the skip validation strategy`, () => {
+    expect(validateIri('https://example.com/a:&#xA;', Strategies.SkipValidation)).toBe(undefined);
+  });
+
+  test(`using the spec strategy`, () => {
+    expect(validateIri('https://example.com/a:&#xA;', Strategies.Specification)).toBeInstanceOf(NotImplementedError);
+  });
+});
+
